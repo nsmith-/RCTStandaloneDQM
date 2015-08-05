5c9dfeac3b0bbe8aff3d02438ee97edf0617170f
Nick Smith <ncsmith6@wisc.edu>
2015-08-05 14:09:55 +0200

    Move L1TPUM to central DQM area

--- src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-// TODO: if we move to online DQM, switch
-//#include "DQM/L1TMonitor/interface/L1TPUM.h"
-#include "DQM/RCTStandaloneDQM/interface/L1TPUM.h"
-DEFINE_FWK_MODULE(L1TPUM);
-
