cb27f200aa428a4d4c53e57547a720c8bca1107c
Nick Smith <ncsmith6@wisc.edu>
2015-08-13 14:46:13 +0200

    Change CTP7 digi source name to rctDigis

--- python/L1TRCToffline_cff.py
+++ python/L1TRCToffline_cff.py
@@ -11,7 +11,7 @@ from L1Trigger.HardwareValidation.L1HardwareValidation_cff import *
 from L1Trigger.Configuration.L1Config_cff import *
 
 from DQM.L1TMonitor.L1TRCT_cfi import *
-l1tRct.rctSource = 'rctHwDigis'
+l1tRct.rctSource = 'rctDigis'
 l1tRct.gctSource = 'gctDigis'
 
 l1tRctfromGCT = l1tRct.clone()
@@ -19,7 +19,7 @@ l1tRctfromGCT.rctSource = 'gctDigis'
 l1tRctfromGCT.HistFolder = cms.untracked.string('L1T/L1TRCT_FromGCT')
 
 from DQM.L1TMonitor.L1TdeRCT_cfi import *
-l1TdeRCT.rctSourceData = 'rctHwDigis'
+l1TdeRCT.rctSourceData = 'rctDigis'
 l1TdeRCT.gctSourceData = 'gctDigis'
 l1TdeRCT.rctSourceEmul = 'valRctDigis'
 
@@ -31,7 +31,7 @@ l1TdeRCTfromGCT.HistFolder = cms.untracked.string('L1TEMU/L1TdeRCT_FromGCT')
 from DQM.RCTStandaloneDQM.L1TPUM_cfi import *
 
 # Trim some unnecessary steps
-RawToDigi = cms.Sequence(rctHwDigis+gctDigis+gtDigis+ecalDigis+hcalDigis+scalersRawToDigi)
+RawToDigi = cms.Sequence(rctDigis+gctDigis+gtDigis+ecalDigis+hcalDigis+scalersRawToDigi)
 L1HardwareValidation = cms.Sequence(deEcal+deHcal+deRct+deGct)
 
 rctdqm = cms.Sequence(
