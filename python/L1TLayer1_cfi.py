fd7397d15ef024d78f473021f88c84778a0e5163
gomber <bhawna.gomber@gmail.com>
2015-09-30 13:26:43 -0500

    Added new varaibles

--- python/L1TLayer1_cfi.py
+++ python/L1TLayer1_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 l1tLayer1 = cms.EDAnalyzer("L1TLayer1",
-    ecalTPSourceData = cms.InputTag("ecalDigis","EcalTriggerPrimitives"),
-    hcalTPSourceData = cms.InputTag("hcalDigis"),
-    ecalTPSourceEmul = cms.InputTag("valEcalTriggerPrimitiveDigis"),
-    hcalTPSourceEmul = cms.InputTag("valHcalTriggerPrimitiveDigis"),
+    ecalTPSourceRecd = cms.InputTag("ecalDigis","EcalTriggerPrimitives"),
+    hcalTPSourceRecd = cms.InputTag("hcalDigis"),
+    ecalTPSourceSent = cms.InputTag("ecalDigis","EcalTriggerPrimitives"),
+    hcalTPSourceSent = cms.InputTag("hcalDigis"),
     histFolder = cms.string('L1TEMU/L1TLayer1'),
 )
